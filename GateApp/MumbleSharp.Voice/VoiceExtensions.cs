2b4111f460af5b6f4a3a5ea3fe39b64c3a288bb4
Roman Buldygin <froooks@gmail.com>
2019-04-12 17:25:15 +0300

    Начал работу над голосовой подсистемой

---
+++ GateApp/MumbleSharp.Voice/VoiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MumbleSharp.Packets;
+using System;
+
+namespace MumbleSharp.Voice
+{
+    public static class VoiceExtensions
+    {
+        public static void UseVoice(this MumbleConnection connection, VoiceSubsystem voiceSubsystem)
+        {
+            connection.RegisterPacketProcessor(PacketType.UserState, voiceSubsystem.UserStateProcessor);
+            connection.RegisterPacketProcessor(PacketType.UserRemove, voiceSubsystem.UserRemoveProcessor);
+        }
+    }
+}
---
+++ GateApp/MumbleSharp.Voice/VoiceSubsystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace MumbleSharp.Voice
+{
+    public class VoiceSubsystem
+    {
+        private readonly ConcurrentDictionary<uint, UserAudioPlayer> _audioBuffers = new ConcurrentDictionary<uint, UserAudioPlayer>();
+
+        internal void UserStateProcessor(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UserRemoveProcessor(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
